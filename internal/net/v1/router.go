16a975c9069e6336b1dd23bfe963da8df574b419
4aykovski <kan89kan@gmail.com>
2024-03-12 13:21:53 +0700

    feat: add middlewares to router

--- internal/net/v1/router.go
+++ internal/net/v1/router.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/4aykovski/task-manager-api/internal/net/v1/middleware"
+	"github.com/4aykovski/task-manager-api/pkg/types"
 	"github.com/go-chi/chi/v5"
 	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 )
 
-func NewMux(log *slog.Logger) *chi.Mux {
+type tokenManager interface {
+	Parse(token string) (string, error)
+	CreateTokensPair(userId string, ttl time.Duration) (types.Tokens, error)
+}
+
+func NewMux(
+	log *slog.Logger,
+	tokenManager tokenManager,
+) *chi.Mux {
 	var (
 		mux = chi.NewMux()
-		mw  = middleware.New()
+		mw  = middleware.New(tokenManager)
 	)
 
 	mux.Use(chiMiddleware.RealIP)
