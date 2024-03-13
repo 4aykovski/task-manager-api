e61622678d2551b7b11ffeac33bca5a08ce9e8e9
4aykovski <kan89kan@gmail.com>
2024-03-13 18:31:52 +0300

    fix: optimize

--- db/migrations/20240313151035_new_private_boards_table.sql
+++ db/migrations/20240313151035_new_private_boards_table.sql
@@ -2,10 +2,10 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS private_boards
 (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(128) NOT NULL,
-    color VARCHAR(128) NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(128) NOT NULL,
+    color   VARCHAR(128) NOT NULL,
+    user_id UUID         NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
